c8ed4375fe4bd5e589f22d0fc0b87975496e50e5
Cynthia Lam <cynthia-lam@outlook.com>
2025-01-19 23:18:53 -0500

    Display call data in activity feed

--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -1,7 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import fetchCalls from '../../routes/useCalls';
+
 const ActivityFeed = () => {
+  const [calls, setCalls] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const data = await fetchCalls();
+        setCalls(data);
+        setLoading(false);
+    };
+
+    fetchData();
+  }, []); // dependencies - just empty array
+
   return (
     <div>
-      Activity Feed here
+      <ul>
+        {calls.map((call) => (
+          <li key={call.id}>
+            {call.from}
+            {call.direction}
+            {call.duration}
+            {call.call_type}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
