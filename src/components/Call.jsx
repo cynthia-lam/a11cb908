05ddb82450313bc6ef5503890fa4d050191e697d
Cynthia Lam <cynthia-lam@outlook.com>
2025-01-20 03:32:00 -0500

    Add info to modal

--- src/components/Call.jsx
+++ src/components/Call.jsx
@@ -3,7 +3,7 @@ import Modal from "./Modal.jsx";
 
 const Call = ({ data }) => {
   const { id, created_at, direction, from, to, via, duration, is_archived, call_type } = data;
-  const[modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const toggleModal = () => {
     setModalOpen(!modalOpen);
@@ -16,6 +16,16 @@ const Call = ({ data }) => {
       {duration}
       <Modal isOpen={modalOpen} toggleModal={toggleModal}>
         <button onClick={toggleModal}>X</button>
+        <br />
+        <div>ID: {id}</div>
+        <div>Created At: {created_at}</div>
+        <div>Direction: {direction}</div>
+        <div>From: {from}</div>
+        <div>To: {to}</div>
+        <div>Via: {via}</div>
+        <div>Duration: {duration}</div>
+        <div>Is Archived: {is_archived ? 'Yes' : 'No'}</div>
+        <div>Call Type: {call_type}</div>
       </Modal>
     </li>
   );
