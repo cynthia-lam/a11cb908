6a33a3770dec57d60d3361d2a642a1f277c9bf78
Cynthia Lam <cynthia-lam@outlook.com>
2025-01-20 03:13:52 -0500

    Implement Modal

--- src/components/Call.jsx
+++ src/components/Call.jsx
@@ -1,11 +1,22 @@
-const Call = ({data}) => {
-  const {id, created_at, direction, from, to, via, duration, is_archived, call_type} = data;
+import React, { useState, useEffect } from 'react';
+import Modal from "./Modal.jsx";
+
+const Call = ({ data }) => {
+  const { id, created_at, direction, from, to, via, duration, is_archived, call_type } = data;
+  const[modalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  };
 
   return (
-    <li>
+    <li onClick={toggleModal}>
       {call_type} call from {from}
       {direction}
       {duration}
+      <Modal isOpen={modalOpen} toggleModal={toggleModal}>
+        <button onClick={toggleModal}>X</button>
+      </Modal>
     </li>
   );
 };
