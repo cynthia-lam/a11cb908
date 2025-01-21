a468b065570cf56daea4db741d4b9d935626ad90
Cynthia Lam <cynthia-lam@outlook.com>
2025-01-20 20:11:22 -0500

    Move styling from Modal.jsx to modal.css

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,34 +1,11 @@
+import '../css/modal.css';
+
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div
-      onClick={onClose}
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        background: "rgba(0, 0, 0, 0.5)",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        zIndex: 1000
-      }}
-    >
-      <div
-        style={{
-          background: "white",
-          height: 150,
-          width: 240,
-          margin: "auto",
-          padding: "2%",
-          border: "2px solid #000",
-          borderRadius: "10px",
-          boxShadow: "2px solid black",
-        }}
-      >
+    <div className='modal' onClick={onClose}>
+      <div className='children-container'>
         {children}
       </div>
     </div>
