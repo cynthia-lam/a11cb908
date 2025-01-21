62485ebf7fb213f1e1ec31320dd7ec4e2e57911f
Cynthia Lam <cynthia-lam@outlook.com>
2025-01-21 02:26:29 -0500

    Add back public and build

--- src/components/CallList.jsx
+++ src/components/CallList.jsx
@@ -95,18 +95,18 @@ const CallList = ({ filter }) => {
   return (
     <div className='call-list'>
       <div className='call-list-top'>
-        <img src='/assets/scroll-top.png' className="scroll-top-button"></img>
+        <img src='public/assets/scroll-top.png' className="scroll-top-button"></img>
         <button
           className='archive-all-button'
           onClick={archiveAll}>
           {filter === 'ActivityFeed' ?
             <>
-              <img src="/assets/archive.png" className="call-action-all-logo" />
+              <img src="public/assets/archive.png" className="call-action-all-logo" />
               ALL
             </>
             :
             <>
-              <img src="/assets/unarchive.png" className="call-action-all-logo" />
+              <img src="public/assets/unarchive.png" className="call-action-all-logo" />
               ALL
             </>
           }
@@ -133,10 +133,10 @@ const CallList = ({ filter }) => {
       )}
     </div>
       <div className='footer'>
-          <img src='/assets/foot-icon-1.png' className='footer-icon'/>
-          <img src='/assets/foot-icon-2.png' className='footer-icon'/>
-          <img src='/assets/foot-icon-3.png' className='footer-icon'/>
-          <img src='/assets/foot-icon-4.png' className='footer-icon'/>
+          <img src='public/assets/answered-call.png' className='footer-icon'/>
+          <img src='public/assets/foot-icon-2.png' className='footer-icon'/>
+          <img src='public/assets/foot-icon-3.png' className='footer-icon'/>
+          <img src='public/assets/foot-icon-4.png' className='footer-icon'/>
       </div>
     </div>
   );
